447f9473a327e1e487fef1e50852563437117cf1
David Bootle <d.bootle18@psaschool.org>
2021-09-15 21:31:32 -0400

    created functioning canvas

--- app.js
+++ app.js
@@ -8,3 +8,12 @@ document.body.appendChild(app.view); // add canvas to the dom
 let coin = PIXI.Sprite.from('assets/coin.png');
 
 app.stage.addChild(coin);
+
+// add a variable to count up the seconds our demo has been running
+let elapsed = 0.0;
+// tell the application ticker to run a new callback every frame, passing in the amount of time that has passed since the last tick.
+app.ticker.add((delta) => {
+    elapsed += delta;
+    // update the sprite's X position based on the cosine of our elapsed time. We divide by 50 to slow down the animation a bit
+    coin.x = 100.0 + Math.cos(elapsed/50.0) * 100.0;
+});
